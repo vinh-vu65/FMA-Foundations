a1c6d939768968792dad61e03dc507711f563544
Vinh Vu <vinh.vu65@gmail.com>
2022-03-09 13:45:26 +1100

    Remove leap year program from menu

--- BackToBasics/Menu.cs
+++ BackToBasics/Menu.cs
@@ -10,7 +10,6 @@ namespace Foundation
         SumOrProduct,
         MultiplicationTable,
         GuessingGame,
-        LeapYear,
         FizzBuzz,
         Continue
     }
@@ -24,8 +23,7 @@ namespace Foundation
             Console.WriteLine("3. Sum Or Product");
             Console.WriteLine("4. Multiplication Table");
             Console.WriteLine("5. Guessing Game");
-            Console.WriteLine("6. Leap Year");
-            Console.WriteLine("7. FizzBuzz");
+            Console.WriteLine("6. FizzBuzz");
         }
         public MenuOption ReadMenuOption()
         {
@@ -33,9 +31,9 @@ namespace Foundation
             bool tryParse;
             do 
             {
-                Console.Write("Enter a number from 1 - 7 to make your choice or enter 0 to quit:");
+                Console.Write("Enter a number from 1 - 6 to make your choice or enter 0 to quit:");
                 tryParse = Int32.TryParse(Console.ReadLine(), out option);
-            } while ((option < 0 || option > 7) || (!tryParse));
+            } while ((option < 0 || option > 6) || (!tryParse));
             return (MenuOption)(option);
         }
         public MenuOption PrintFinishMessage()
