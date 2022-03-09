e6e216dcf371571861a56bac4c813e21633661bf
Vinh Vu <vinh.vu65@gmail.com>
2022-03-09 13:44:58 +1100

    Add guessing game program

---
+++ BackToBasics/GuessingGame.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Foundation
+{
+    public class GuessingGame
+    {
+        public GuessingGame()
+        {
+            PrintInitialMessage();
+            int input = HandleInput();
+            RunGuessingGame(input);
+        }
+        public void PrintInitialMessage()
+        {
+            Console.WriteLine("You have chosen to run Guessing Game");
+            Console.WriteLine("Please choose a number (n). The program will choose a random number between 1 and n and you have to guess what the number is");
+            Console.WriteLine("Good luck!");
+        }
+        public int HandleInput()
+        {
+            int userInput;
+            do 
+            {
+                Console.WriteLine("Please enter a number greater than zero: ");
+                Int32.TryParse(Console.ReadLine(), out userInput);
+            } while (userInput <= 0);
+            return userInput;
+        }
+        public void RunGuessingGame(int maxNumber)
+        {
+            Random rnd = new Random();
+            int numberToGuess = rnd.Next(1, maxNumber + 1);
+            int userGuess;
+            int minNumber = 1;
+            int guessCount = 0;
+            bool parseInput;
+            do
+            {
+                Console.WriteLine($"Guess a number between {minNumber} and {maxNumber}");
+                parseInput = Int32.TryParse(Console.ReadLine(), out userGuess);
+                if (userGuess > numberToGuess && userGuess < maxNumber)
+                {
+                    Console.WriteLine("That guess was too high, try again.");
+                    maxNumber = userGuess;
+                    guessCount++;
+                }
+                if (userGuess < numberToGuess && userGuess > minNumber)
+                {
+                    Console.WriteLine("That guess was too low, try again.");
+                    minNumber = userGuess;
+                    guessCount++;
+                }
+            } while (!parseInput || userGuess != numberToGuess);
+            guessCount++;
+            Console.WriteLine($"Correct! The number was {numberToGuess}");
+            Console.WriteLine($"You guessed it in {guessCount} tries!");
+        }
+    }
+}
--- BackToBasics/Program.cs
+++ BackToBasics/Program.cs
@@ -32,9 +32,7 @@ namespace Foundation
                     break;
 
                     case MenuOption.GuessingGame:
-                    break;
-
-                    case MenuOption.LeapYear:
+                    var guessingGame = new GuessingGame();
                     break;
 
                     case MenuOption.FizzBuzz:
@@ -51,10 +49,3 @@ namespace Foundation
         }
     }
 }
-
-/*
-TASKS:
-Write a program that prints a multiplication table for numbers up to 12.
-Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
-Write a program that prints the next 20 leap years.
-*/
