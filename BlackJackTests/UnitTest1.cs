30d68ffc3109bf058438f972c8782627eb34a5a8
Vinh Vu <vinh.vu65@gmail.com>
2022-03-21 10:00:22 +1100

    Update Unittest to include inline data

--- BlackJackTests/UnitTest1.cs
+++ BlackJackTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Security;
 using System.Text.RegularExpressions;
@@ -8,16 +9,44 @@ namespace BlackJackTests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void PictureCardReturnValueOf10()
+    [Theory]
+    [InlineData(Card.Value.KING)]
+    [InlineData(Card.Value.QUEEN)]
+    [InlineData(Card.Value.JACK)]
+    public void PictureCardReturnValueOf10(Card.Value value)
     {
         // Arrange:
-        var pictureCard = new Card(Card.Value.KING, Card.Suit.SPADE);
+        var pictureCard = new Card(value, Card.Suit.SPADE);
 
         // Act:
         var result = pictureCard.GetValue();
 
         //Assert:
         Assert.Equal(10, result);
+        
+        // When, Given, Then
     }
 }
+/*
+public class UnitTest2
+{
+    [Fact]
+    public void AceValueEqualsOneWhenBust()
+    {
+        // Arrange:
+        var player = new User()
+        player.Hand = new List<Card>();
+        player.Hand.Add(new Card(Card.Value.ACE, Card.Suit.CLUB));
+        player.Hand.Add(new Card(Card.Value.JACK, Card.Suit.SPADE));
+        player.Hand.Add(new Card(Card.Value.FIVE, Card.Suit.SPADE));
+
+        var engine = new GameEngine();
+
+        // Act:
+        var result = engine.CalculateHandValue(player);
+
+        // Assert:
+        Assert.Equal(16, result);
+    }
+}
+*/
