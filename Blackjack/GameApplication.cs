6b59e47b40825404eb22b1a163b0962524d0a1d5
Vinh Vu <vinh.vu65@gmail.com>
2022-03-22 12:27:20 +1100

    Add run method to handle running of program

--- Blackjack/GameApplication.cs
+++ Blackjack/GameApplication.cs
@@ -2,15 +2,6 @@ namespace BlackJack;
 
 public class GameApplication
 {
-    // Print welcome and start up
-    // Load game engine -> load a deck and shuffle
-    // User is given a hand of 2 cards -> PrintHand + value
-    // PrintOptions() -> Hit() = [player draw from deck] or Stay()
-    // HandleInput()
-    // If Stay() -> Load dealer cards
-    // Dealer automatically hit if value < 17.
-    // Determine Winner
-    // Print Winner message
     public User User;
     public Dealer Dealer;
     public GameEngine GameEngine;
@@ -34,12 +25,51 @@ public class GameApplication
         Console.WriteLine("The game will now begin loading... \n");
     }
 
+    public void Run()
+    {
+        UserTurn();
+        GameEngine.CheckIfBust(User);
+        DealerTurn();
+        GameEngine.CheckIfBust(Dealer);
+        GameEngine.DetermineWinner(User, Dealer);
+        PrintWinnerMessage();
+    }
+    
     public void UserTurn()
     {
-        GameEngine.DealInitialHand(User);
+        GameEngine.DealToPlayer(User);
+        do
+        {
+            GameEngine.DealToPlayer(User);
+            GameEngine.CalculateHandValue(User);
+            GameEngine.CheckIfBust(User);
+            PrintHand(User);
+            if (User.Bust)
+            {
+                return;
+            }
+            GameEngine.HandleInput();
+        } while (!User.Stay);
     }
 
-    public void PrintHand(IPlayer player)
+    public void DealerTurn()
+    {
+        GameEngine.DealToPlayer(Dealer);
+        do
+        {
+            GameEngine.DealToPlayer(Dealer);
+            GameEngine.CalculateHandValue(Dealer);
+            GameEngine.CheckIfBust(Dealer);
+            PrintHand(Dealer);
+            if (Dealer.HandValue > 17)
+            {
+                Dealer.Stay = true;
+            }
+        } while (!Dealer.Stay);
+
+    }
+
+    private void PrintHand(IPlayer player)
     {
         Console.Write($"{player}'s current hand is: ");
         foreach (var card in player.Hand)
@@ -47,5 +77,23 @@ public class GameApplication
             Console.Write(card + " ");
         }
         Console.WriteLine($"\n{player}'s current total is: {player.HandValue}");
+    }
+
+    private void PrintWinnerMessage()
+    {
+        if (GameEngine.DealerWins)
+        {
+            Console.WriteLine("Dealer wins!");
+        }
+
+        if (GameEngine.UserWins)
+        {
+            Console.WriteLine("You beat the dealer!");
+        }
+
+        if (GameEngine.GameTie)
+        {
+            Console.WriteLine("The game has ended in a tie");
+        }
     }
 }
