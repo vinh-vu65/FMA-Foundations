ad10a9a1ac12eedf56b3bbd70de26f8b6e551f30
Vinh Vu <vinh.vu65@gmail.com>
2022-03-31 10:29:46 +1100

    Add updateboard and move validator method

--- TicTacToe/Controller/BoardController.cs
+++ TicTacToe/Controller/BoardController.cs
@@ -1,7 +1,40 @@
+using System.Xml.XPath;
+using TicTacToe.Models;
+
 namespace TicTacToe.Controller;
 
 public class BoardController
 {
     // Verify Input
     // Update Board
+    public GameBoard GameBoard { get; set; }
+    public string UserInput { get; set; }
+
+    public BoardController(GameBoard gameBoard)
+    {
+        GameBoard = gameBoard;
+    }
+
+    public bool IsMoveValid()
+    {
+        foreach (var coord in GameBoard.BoardCoordinates)
+        {
+            if (coord.ToString() == UserInput && coord.IsUsed())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public void UpdateBoard(IPlayer player)
+    {
+        foreach (var coord in GameBoard.BoardCoordinates)
+        {
+            if (coord.ToString() == UserInput)
+            {
+                coord.Value = player.BoardMarker;
+            }
+        }
+    }
 }
