ccffb93d9df39512b94affdf3fa825a6ff9c3cd5
Vinh Vu <vinh.vu65@gmail.com>
2022-03-31 10:26:37 +1100

    Add exception handling when row size is 0 to pass tests

--- TicTacToe/Models/GameBoard.cs
+++ TicTacToe/Models/GameBoard.cs
@@ -2,24 +2,29 @@ namespace TicTacToe.Models;
 
 public class GameBoard
 {
-    public int NumberOfRows { get; set; }
+    public static int NumberOfRows { get; set; }
     public int NumberOfColumns;
-    public List<Coordinates> BoardCoordinates;
+    public List<Cell> BoardCoordinates;
 
     public GameBoard(int numberOfRows)
     {
         NumberOfRows = numberOfRows;
         NumberOfColumns = numberOfRows;
-        BoardCoordinates = new List<Coordinates>();
+        BoardCoordinates = new List<Cell>();
     }
 
     public void LoadInitialBoard()
     {
+        if (NumberOfRows == 0)
+        {
+            throw new Exception();
+        }
+        
         for (int i = 1; i <= NumberOfRows; i++)
         {
             for (int j = 1; j <= NumberOfColumns; j++)
             {
-                var coordinateToAdd = new Coordinates(j, i);
+                var coordinateToAdd = new Cell(j, i);
                 BoardCoordinates.Add(coordinateToAdd);
             }
         }
