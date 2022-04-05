143e01f38291421ea5d1860468d4983153793b7a
Vinh Vu <vinh.vu65@gmail.com>
2022-04-05 13:30:43 +1000

    Remove TurnCoordinates and MovesAccepted properties

--- TicTacToe/Models/IPlayer.cs
+++ TicTacToe/Models/IPlayer.cs
@@ -2,8 +2,6 @@ namespace TicTacToe.Models;
 
 public interface IPlayer
 { 
-    int MovesAccepted { get; set; }
     string BoardMarker { get; set; }
-    public void ChooseCoordinates();
-    public string TurnCoordinates { get; set; }
+    public string ChooseCoordinates();
 }
