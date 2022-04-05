cfe22c84b8ec4fdd14d6c91735afbc83bd77306b
Vinh Vu <vinh.vu65@gmail.com>
2022-04-05 15:07:43 +1000

    Rename Printer method

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,10 +9,10 @@ public static class Program
     public static void Main(string[] args)
     {
         var inputHandler = new InputHandler();
-        Printer.PrintWelcome();
+        Printer.Welcome();
 
         var gameBoard = new GameBoard(GameMessages.DEFAULT_NUMBER_OF_ROWS);
-        Printer.PrintBoard(gameBoard);
+        Printer.GameBoard(gameBoard);
         var playerX = new HumanPlayer("X", gameBoard, inputHandler);
         var playerO = new HumanPlayer("O", gameBoard, inputHandler);
         var controller = new GameBoardController(gameBoard);
