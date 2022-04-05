4c6969a9cef70b3890e7a498c4b6050dd57974f7
Vinh Vu <vinh.vu65@gmail.com>
2022-04-05 13:32:55 +1000

    Working game flow, need to add win condition checking

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace TicTacToe;
+﻿using TicTacToe.Controller;
+using TicTacToe.Models;
+using TicTacToe.View;
+
+namespace TicTacToe;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
+        var inputHandler = new InputHandler();
+        Printer.PrintWelcome();
+
+        var gameBoard = new GameBoard(GameMessages.DefaultNumberOfRows);
+        Printer.PrintBoard(gameBoard);
+        var playerX = new HumanPlayer("X", gameBoard, inputHandler);
+        var playerO = new HumanPlayer("O", gameBoard, inputHandler);
+        var controller = new GameBoardController(gameBoard);
+
+        var flowController = new GameFlowController(controller, gameBoard);
+
+        while (!flowController.Quit)
+        {
+            flowController.PlayerTurn(playerX);
+        
+            Printer.PrintBoard(gameBoard);
+
+            if (flowController.IsBoardFullyUsed())
+            {
+                flowController.Quit = true;
+                return;
+            }
+
+            flowController.PlayerTurn(playerO);
+        
+            Printer.PrintBoard(gameBoard);
+            
+            if (flowController.IsBoardFullyUsed())
+            {
+                flowController.Quit = true;
+                return;
+            }
+        }
         
     }
 }
+
+
 
