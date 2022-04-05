b1ea95b6047906a1bc36c7c09ee6f3d8ba8dcea4
Vinh Vu <vinh.vu65@gmail.com>
2022-04-05 14:44:20 +1000

    Rename variable to follow const naming convention

--- TicTacToe/Constants/GameMessages.cs
+++ TicTacToe/Constants/GameMessages.cs
@@ -7,5 +7,5 @@ public static class GameMessages
     public static string MoveAcceptedMessage = "Move accepted, here's the current board:";
     public static string InvalidMoveMessage = "Oh no, a piece is already at this place! Try again...";
     public static string GameTieMessage = "The game has ended in a tie.";
-    public const int DefaultNumberOfRows = 3;
+    public const int DEFAULT_NUMBER_OF_ROWS = 3;
 }
