8c91ae06e04bbcf6aef535ac93fa96a1a7a25f82
Vinh Vu <vinh.vu65@gmail.com>
2022-04-05 13:31:15 +1000

    Renamed exception

--- TicTacToe/Exceptions/InvalidCoordinateException.cs
+++
@@ -1,6 +0,0 @@
-namespace TicTacToe.Exceptions;
-
-public class InvalidCoordinateException : Exception
-{
-    public InvalidCoordinateException() : base("Invalid input, enter co-ordinates in x,y format (ie. 2,1): ") {}
-}
