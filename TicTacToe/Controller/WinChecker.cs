858fd458f87c60602afad46761c2fb25534bd3cc
Vinh Vu <vinh.vu65@gmail.com>
2022-04-06 12:43:28 +1000

    Extract win checking methods to their own class

---
+++ TicTacToe/Controller/WinChecker.cs
@@ -0,0 +1,104 @@
+using TicTacToe.Models;
+
+namespace TicTacToe.Controller;
+
+public class WinChecker
+{
+    public string Winner { get; private set; }
+    private readonly GameBoard _gameBoard;
+    
+    public WinChecker(GameBoard gameBoard)
+    {
+        _gameBoard = gameBoard;
+    }
+
+    public void PerformWinCheck()
+    {
+        CheckRowsForWinner();
+        CheckColumnsForWinner();
+        CheckDiagonalForWinner();
+    }
+    
+    public void CheckRowsForWinner()
+    {
+        for (int i = 0; i < _gameBoard.BoardCoordinates.Count; i += _gameBoard.Size)
+        {
+            var firstValue = _gameBoard.BoardCoordinates[i].Value;
+            var isWinningRow = true;
+            for (int j = 1; j < _gameBoard.Size; j++)
+            {
+                if (_gameBoard.BoardCoordinates[i + j].Value != firstValue)
+                {
+                    isWinningRow = false;
+                    break;
+                }
+            }
+
+            if (isWinningRow)
+            {
+                Winner = firstValue;
+                return;
+            }
+        }
+    }
+
+    public void CheckColumnsForWinner()
+    {
+        for (int i = 0; i < _gameBoard.Size; i++)
+        {
+            var firstValue = _gameBoard.BoardCoordinates[i].Value;
+            var isWinningColumn = true;
+            for (int j = _gameBoard.Size; j < _gameBoard.BoardCoordinates.Count; j += _gameBoard.Size)
+            {
+                if (_gameBoard.BoardCoordinates[i + j].Value != firstValue)
+                {
+                    isWinningColumn = false;
+                    break;
+                }
+            }
+
+            if (isWinningColumn)
+            {
+                Winner = firstValue;
+                return;
+            }
+        }
+    }
+
+    public void CheckDiagonalForWinner()
+    {
+        /* Following index of the coordinates:
+        *
+        *  For a R to L diagonal win, follows pattern of multiples of GameBoard.Size - 1
+        * 
+        *  For L to R diagonal win, follows pattern of multiples of GameBoard.Size + 1 (starting from zero)
+        */
+        var leftToRightDiagonal = _gameBoard.BoardCoordinates[0].Value;
+        var rightToLeftDiagonal = _gameBoard.BoardCoordinates[_gameBoard.Size - 1].Value;
+        var isLeftToRightWinningDiagonal = true;
+        var isRightToLeftWinningDiagonal = true;
+
+        for (int i = 1; i < _gameBoard.Size; i++)
+        {
+            if (rightToLeftDiagonal != _gameBoard.BoardCoordinates[(i + 1) * (_gameBoard.Size - 1)].Value)
+            {
+                isRightToLeftWinningDiagonal = false;
+            }
+
+            if (leftToRightDiagonal != _gameBoard.BoardCoordinates[i * (_gameBoard.Size + 1)].Value)
+            {
+                isLeftToRightWinningDiagonal = false;
+            }
+        }
+
+        if (isLeftToRightWinningDiagonal)
+        {
+            Winner = leftToRightDiagonal;
+        }
+
+        if (isRightToLeftWinningDiagonal)
+        {
+            Winner = rightToLeftDiagonal;
+        }
+    }
+}
